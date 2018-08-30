f7c0ef676c777120c52a78b09a3273e7908c0436
Zhengyang Zhao <zyzhao@thoughtworks.com>
2018-08-30 14:27:18 +0800

    update to 1.0.5

--- exports/hammer/lib/hammer.directive.d.ts
+++ exports/hammer/lib/hammer.directive.d.ts
@@ -1,14 +1,16 @@
-import { ElementRef, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { ElementRef, OnChanges, SimpleChanges, OnDestroy, EventEmitter } from '@angular/core';
 import { NgHammerConfig } from './model';
 export declare class HammerDirective implements OnChanges, OnDestroy {
     private el;
     ngHammer: NgHammerConfig;
+    eventTriggered: EventEmitter<{}>;
     elementInstance: any;
     constructor(el: ElementRef);
     ngOnChanges(changes: SimpleChanges): void;
     ngOnDestroy(): void;
     createHammerManager(): void;
     createHammerRecognizer(): void;
+    private trigger(e);
     private capitalize(str);
     private guardDirections(direction);
 }
